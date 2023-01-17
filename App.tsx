f07646606f9a68147141c1026da2301d70fecd61
Saullo <saullorhamon@gmail.com>
2023-01-17 06:12:37 -0300

    initial commit mobile project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if(!fontsLoaded){
+    return (<Loading />)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent />
     </View>
   );
 }
@@ -13,8 +24,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: '#12dddd',
+    fontFamily: 'Inter_800ExtraBold'
+
+  }
 });

92bd70a5f0276a1d820049c400efbc92517965fe
Saullo <saullorhamon@gmail.com>
2023-01-21 07:53:00 -0300

    day 02

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from '@expo-google-fonts/inter';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
+import './src/lib/dayjs';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
